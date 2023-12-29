d14bdcc2c72fb733e6d34255918ae239859aed59
Peter Kwok <peter.kwok94@gmail.com>
2023-12-29 18:04:00 -0500

    Save progress: 1410.1

--- fortune.sh
+++ fortune.sh
@@ -7,7 +7,11 @@ RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again late
 N=$(( RANDOM % 6 ))
 
 GET_FORTUNE() {
-    echo Ask a yes or no question:
+    if [[ ! $1 ]]
+      then
+      echo Ask a yes or no question:
+    fi 
+      
     read QUESTION
 }
 
