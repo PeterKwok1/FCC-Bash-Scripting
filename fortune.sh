6f84b806f63367e625b8e6538b282c8499f46759
Peter Kwok <peter.kwok94@gmail.com>
2023-12-29 19:17:25 -0500

    finished fortune telling bash script

--- fortune.sh
+++ fortune.sh
@@ -4,8 +4,9 @@ echo -e "\n~~ Fortune Teller ~~\n"
 
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 
-N=$(( RANDOM % 6 ))
+N=$(( $RANDOM % 6 ))
 
+: '
 GET_FORTUNE() {
     if [[ ! $1 ]]
       then
@@ -13,12 +14,57 @@ GET_FORTUNE() {
       else
         echo Try again. Make sure it ends with a question mark:
     fi 
-    
+    read QUESTION
     echo -e "\n${RESPONSES[$N]}"
 }
 
-read QUESTION
 until [[ $QUESTION =~ \?$ ]]
-do
-  GET_FORTUNE 
-done
+  do
+    GET_FORTUNE again
+  done
+'
+
+# My version 
+: '
+1. Ask for question 
+2. Get question
+3. If not a question, explain, get question again.
+'
+
+: '
+GET_FORTUNE_2() {
+  echo Ask a yes or no question:
+  until [[ $QUESTION =~ \?$ ]]
+    do 
+      read QUESTION
+      if ! [[ $QUESTION =~ \?$ ]]
+        then
+          echo Try again. Make sure it ends with a question mark:
+      fi
+    done
+  echo -e "\n${RESPONSES[$N]}"
+}
+
+GET_FORTUNE_2
+'
+
+# Version 2 
+: '
+1. Ask for question 
+2. Get question
+3. If not a question, explain, repeat.
+'
+
+GET_FORTUNE_2() {
+  echo Ask a yes or no question:
+  read QUESTION
+  if ! [[ $QUESTION =~ \?$ ]]
+    then 
+      echo Try again. Make sure it ends with a question mark:
+      GET_FORTUNE_2
+    else
+    echo -e "\n${RESPONSES[$N]}"
+    fi
+}
+
+GET_FORTUNE_2
